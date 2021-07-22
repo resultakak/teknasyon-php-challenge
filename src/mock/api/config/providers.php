55b79017bee0ae45a0c2f15768d1810d4356bbc3
Resul Takak <resultakak@gmail.com>
2021-07-23 01:26:53 +0300

    Create providers.php

---
+++ src/mock/api/config/providers.php
@@ -0,0 +1,14 @@
+//use Phalcon\Api\Providers\CacheDataProvider;
+//use Phalcon\Api\Providers\ConfigProvider;
+//use Phalcon\Api\Providers\DatabaseProvider;
+//use Phalcon\Api\Providers\ErrorHandlerProvider;
+//use Phalcon\Api\Providers\LoggerProvider;
+//use Phalcon\Api\Providers\ModelsMetadataProvider;
+//use Phalcon\Api\Providers\RequestProvider;
+//use Phalcon\Api\Providers\ResponseProvider;
+//    ConfigProvider::class,
+//    LoggerProvider::class,
+//    ModelsMetadataProvider::class,
+//    RequestProvider::class,
+//    ResponseProvider::class,
+//    CacheDataProvider::class,
