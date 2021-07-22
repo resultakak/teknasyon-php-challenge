b2ec08ed3418e0a6945f9e5298d5ea0219111388
Resul Takak <resultakak@gmail.com>
2021-07-23 02:21:20 +0300

    Update providers.php

--- src/mock/api/config/providers.php
+++ src/mock/api/config/providers.php
@@ -1,15 +1,17 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 use App\Providers\CacheDataProvider;
+use App\Providers\ConfigProvider;
 use App\Providers\DatabaseProvider;
 use App\Providers\RouterProvider;
 use App\Providers\SessionProvider;
 
 return [
+    CacheDataProvider::class,
+    ConfigProvider::class,
     DatabaseProvider::class,
     RouterProvider::class,
-    CacheDataProvider::class,
     SessionProvider::class,
 ];
