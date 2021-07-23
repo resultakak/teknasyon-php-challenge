0845faa36ae2149ec560df0c645b14d753d2b0d0
Resul Takak <resultakak@gmail.com>
2021-07-23 23:36:19 +0300

    Update UserSeeder.php

--- src/mock/db/seeds/UserSeeder.php
+++ src/mock/db/seeds/UserSeeder.php
@@ -4,8 +4,19 @@ use Phinx\Seed\AbstractSeed;
 
 class UserSeeder extends AbstractSeed
 {
+
     public function run()
     {
+        $faker = Faker\Factory::create();
+        $data = [];
+        for ($i = 0; $i < 100; $i++) {
+            $data[] = [
+                'name'     => $faker->userName,
+                'password' => sha1($faker->password),
+                'created'  => date('Y-m-d H:i:s'),
+            ];
+        }
 
+        $this->table('credentials')->insert($data)->saveData();
     }
 }
