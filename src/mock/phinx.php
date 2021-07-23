177e27fb0d25d2e42787ca917ba010510bc5f74c
Resul Takak <resultakak@gmail.com>
2021-07-23 23:43:03 +0300

    Update phinx.php

--- src/mock/phinx.php
+++ src/mock/phinx.php
@@ -1,41 +1,26 @@
 <?php
 
-return
-[
-    'paths' => [
+$env = [
+    'adapter' => 'mysql',
+    'host'    => getenv('DB_HOST'),
+    'name'    => getenv('DB_DATABASE'),
+    'user'    => getenv('DB_USERNAME'),
+    'pass'    => getenv('DB_PASSWORD'),
+    'port'    => '3306',
+    'charset' => 'utf8',
+];
+
+return [
+    'paths'         => [
         'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+        'seeds'      => '%%PHINX_CONFIG_DIR%%/db/seeds'
     ],
-    'environments' => [
+    'environments'  => [
         'default_migration_table' => 'phinxlog',
-        'default_environment' => 'development',
-        'production' => [
-            'adapter' => 'mysql',
-            'host' => getenv('DB_HOST'),
-            'name' => getenv('DB_DATABASE'),
-            'user' => getenv('DB_USERNAME'),
-            'pass' => getenv('DB_PASSWORD'),
-            'port' => '3306',
-            'charset' => 'utf8',
-        ],
-        'development' => [
-            'adapter' => 'mysql',
-            'host' => getenv('DB_HOST'),
-            'name' => getenv('DB_DATABASE'),
-            'user' => getenv('DB_USERNAME'),
-            'pass' => getenv('DB_PASSWORD'),
-            'port' => '3306',
-            'charset' => 'utf8',
-        ],
-        'testing' => [
-            'adapter' => 'mysql',
-            'host' => getenv('DB_HOST'),
-            'name' => getenv('DB_DATABASE'),
-            'user' => getenv('DB_USERNAME'),
-            'pass' => getenv('DB_PASSWORD'),
-            'port' => '3306',
-            'charset' => 'utf8',
-        ]
+        'default_environment'     => 'development',
+        'production'              => $env,
+        'development'             => $env,
+        'testing'                 => $env
     ],
     'version_order' => 'creation'
 ];
