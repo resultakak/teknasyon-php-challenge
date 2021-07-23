e6331db5bb74b11df0c2a133fcd878950c6611b0
Resul Takak <resultakak@gmail.com>
2021-07-23 12:46:05 +0300

    Create Request.php

---
+++ src/mock/src/Http/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http;
+
+use Phalcon\Http\Request as PhRequest;
+
+class Request extends PhRequest
+{
+    public function getReceiptHash()
+    {
+        $data = $this->getJsonRawBody();
+        return true == isset($data->receipt) ? $data->receipt : false;
+    }
+}
