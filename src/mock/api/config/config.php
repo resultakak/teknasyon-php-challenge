a86e204ed17f2167d516301999a6c5ff78df8ab9
Resul Takak <resultakak@gmail.com>
2021-07-23 23:10:15 +0300

    Update config.php

--- src/mock/api/config/config.php
+++ src/mock/api/config/config.php
@@ -1,14 +1,32 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
+
+use function App\Core\envValue;
 
 return [
-    'app'        => [
-        'version'      => getenv('VERSION'),
-        'timezone'     => getenv('APP_TIMEZONE'),
-        'debug'        => getenv('APP_DEBUG', false),
-        'env'          => getenv('APP_ENV'),
-        'baseUri'      => getenv('APP_BASE_URI'),
+    'app'   => [
+        'version'      => envValue('VERSION', time()),
+        'timezone'     => envValue('APP_TIMEZONE', 'UTC'),
+        'debug'        => envValue('APP_DEBUG', false),
+        'env'          => envValue('APP_ENV', 'development'),
+        'devMode'      => true,
+        'baseUri'      => envValue('APP_BASE_URI'),
+        'supportEmail' => envValue('APP_SUPPORT_EMAIL'),
         'time'         => microtime(true),
+    ],
+    'db'    => [
+        'host'     => getenv('DB_HOST'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+        'dbname'   => getenv('DB_DATABASE')
+    ],
+    'cache' => [
+        'uniqueId'          => 'mock_cache',
+        'defaultSerializer' => 'Json',
+        'lifetime'          => 7200,
+        'host'              => getenv('REDIS_HOST'),
+        'port'              => getenv('REDIS_PORT'),
+        'index'             => 1,
     ],
 ];
