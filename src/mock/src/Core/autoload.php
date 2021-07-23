a74857f0a3d8b7b560b04a35e2291693257ca4e3
Resul Takak <resultakak@gmail.com>
2021-07-23 23:17:59 +0300

    Update autoload.php

--- src/mock/src/Core/autoload.php
+++ src/mock/src/Core/autoload.php
@@ -10,7 +10,7 @@ require __DIR__.'/functions.php';
 
 $loader = new Loader;
 $namespaces = [
-    'App\Api\Controllers' => appPath('/api/controllers'),
+    //'App\Api\Controllers' => appPath('/api/controllers'),
 ];
 
 $loader->registerNamespaces($namespaces);
