9a278372e0222d9f66a17ec5f1db539f14502177
Resul Takak <resultakak@gmail.com>
2021-07-23 12:48:06 +0300

    Create Receipt.php

---
+++ src/mock/src/Receipt/Receipt.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Receipt;
+
+use Phalcon\Filter\FilterFactory;
+
+class Receipt
+{
+
+    public $locator;
+
+    public function __construct()
+    {
+        $factory = new FilterFactory;
+        $this->locator = $factory->newInstance();
+    }
+
+    public function verify($receipt)
+    {
+        $receipt = $this->validation($receipt);
+
+        if (true === $receipt) {
+            return $receipt;
+        }
+
+        return false;
+    }
+
+    public function validation($receipt)
+    {
+        if (empty($receipt) || ! isset($receipt)) {
+            return false;
+        }
+
+        return $this->locator->sanitize($receipt, 'string');
+    }
+}
