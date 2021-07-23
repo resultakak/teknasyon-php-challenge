ce077283d227f5d5bffb43b737cfa5470dd07a97
Resul Takak <resultakak@gmail.com>
2021-07-23 12:48:30 +0300

    Update providers.php

--- src/mock/api/config/providers.php
+++ src/mock/api/config/providers.php
@@ -6,22 +6,22 @@ use App\Providers\ConfigProvider;
 use App\Providers\LoggerProvider;
 use App\Providers\ErrorHandlerProvider;
 use App\Providers\DatabaseProvider;
-use App\Providers\ModelsMetadataProvider;
 use App\Providers\RequestProvider;
 use App\Providers\ResponseProvider;
 use App\Providers\RouterProvider;
 use App\Providers\CacheDataProvider;
 use App\Providers\SessionProvider;
+use App\Providers\ReceiptProvider;
 
 return [
     ConfigProvider::class,
     LoggerProvider::class,
-    ErrorHandlerProvider::class,
+    //ErrorHandlerProvider::class,
     DatabaseProvider::class,
-    ModelsMetadataProvider::class,
     RequestProvider::class,
     ResponseProvider::class,
     RouterProvider::class,
     CacheDataProvider::class,
     SessionProvider::class,
+    ReceiptProvider::class,
 ];
