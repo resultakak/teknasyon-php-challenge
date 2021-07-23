23080ea611e3ad1bd0bb350e21ef9d61edfe5cad
Resul Takak <resultakak@gmail.com>
2021-07-23 23:36:12 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ docker-compose up -d --build
 docker-compose exec api composer install;
 docker-compose exec worker composer install;
 docker-compose exec mock composer install;
+docker-compose exec mock vendor/bin/phinx seed:run;
