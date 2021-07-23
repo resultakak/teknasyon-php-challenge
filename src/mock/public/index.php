fb07ba4fa672b54e5df7cd6f0e380a074581bf47
Resul Takak <resultakak@gmail.com>
2021-07-23 12:48:53 +0300

    Update index.php

--- src/mock/public/index.php
+++ src/mock/public/index.php
@@ -5,19 +5,13 @@ declare(strict_types=1);
 error_reporting(E_ALL);
 
 use App\Bootstrap\Api;
-use Phalcon\Mvc\Micro\Exception;
 
 define('BASE_PATH', dirname(__DIR__));
 
-try {
-    require_once __DIR__ . '/../src/Core/autoload.php';
+require_once __DIR__ . '/../src/Core/autoload.php';
 
-    $app = new Api();
+$app = new Api();
 
-    $app->setup();
+$app->setup();
 
-    $app->run();
-
-} catch (Exception $ex) {
-    echo $ex->getMessage();
-}
+$app->run();
