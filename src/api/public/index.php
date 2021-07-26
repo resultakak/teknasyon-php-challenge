979e5201276c9146b819628d2d2d20d36efa77df
Resul Takak <resultakak@gmail.com>
2021-07-26 20:01:01 +0300

    Create index.php

---
+++ src/api/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(E_ALL);
+
+use Api\Bootstrap\Api;
+
+define('BASE_PATH', dirname(__DIR__));
+
+require_once __DIR__ . '/../src/Core/autoload.php';
+
+$app = new Api();
+
+$app->setup();
+
+$app->run();
