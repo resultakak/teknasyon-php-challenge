a5794d56cf7c97d6c892948b66665a3311b58f85
Resul Takak <resultakak@gmail.com>
2021-07-26 20:00:22 +0300

    Create config.php

---
+++ src/api/config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use function Api\Core\envValue;
+
+return [
+    'app'   => [
+        'version'      => envValue('VERSION', time()),
+        'timezone'     => envValue('APP_TIMEZONE', 'UTC'),
+        'debug'        => envValue('APP_DEBUG', false),
+        'env'          => envValue('APP_ENV', 'development'),
+        'devMode'      => true,
+        'baseUri'      => envValue('APP_BASE_URI'),
+        'supportEmail' => envValue('APP_SUPPORT_EMAIL'),
+        'time'         => microtime(true),
+    ],
+    'db'    => [
+        'host'     => getenv('DB_HOST'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+        'dbname'   => getenv('DB_DATABASE')
+    ],
+    'cache' => [
+        'uniqueId'          => 'api_cache',
+        'defaultSerializer' => 'Json',
+        'lifetime'          => 7200,
+        'host'              => getenv('REDIS_HOST'),
+        'port'              => getenv('REDIS_PORT'),
+        'index'             => 1,
+    ]
+];
