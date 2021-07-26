3be24ff38053cf3de04a07b5f4d8c66e403c0b9c
Resul Takak <resultakak@gmail.com>
2021-07-26 21:55:13 +0300

    Create ResponseProvider.php

---
+++ src/api/src/Providers/ResponseProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Providers;
+
+use Api\Http\Response;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Di\DiInterface;
+
+class ResponseProvider implements ServiceProviderInterface
+{
+    /**
+     * @param DiInterface $container
+     */
+    public function register(DiInterface $container): void
+    {
+        $container->setShared(
+            'response',
+            function () {
+                $response = new Response();
+
+                $response
+                    ->setStatusCode(200)
+                    ->setContentType('application/vnd.api+json', 'UTF-8')
+                ;
+
+                return $response;
+            }
+        );
+    }
+}
