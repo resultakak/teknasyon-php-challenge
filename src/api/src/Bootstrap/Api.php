cd746e11a4fed011866019e6c09212f3c6951302
Resul Takak <resultakak@gmail.com>
2021-07-26 20:01:41 +0300

    Create Api.php

---
+++ src/api/src/Bootstrap/Api.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Api\Bootstrap;
+
+class Api
+{
+    /**
+     * @return $this
+     */
+    public function setup()
+    {
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function run()
+    {
+        return $this;
+    }
+}
