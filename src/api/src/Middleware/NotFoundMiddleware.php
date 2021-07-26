dd1a22729bf194115f7874fe739cbc895937a777
Resul Takak <resultakak@gmail.com>
2021-07-26 21:55:06 +0300

    Create NotFoundMiddleware.php

---
+++ src/api/src/Middleware/NotFoundMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Middleware;
+
+use Api\Traits\ResponseTrait;
+use Phalcon\Di\Injectable;
+use Phalcon\Mvc\Micro;
+use Phalcon\Mvc\Micro\MiddlewareInterface;
+
+class NotFoundMiddleware extends Injectable implements MiddlewareInterface
+{
+    use ResponseTrait;
+
+    public function beforeNotFound()
+    {
+        $this->halt(
+            $this->application,
+            $this->response::NOT_FOUND,
+            $this->response->getHttpCodeDescription($this->response::NOT_FOUND)
+        );
+
+        return false;
+    }
+
+    public function call(Micro $mock)
+    {
+        return true;
+    }
+}
