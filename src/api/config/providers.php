3b9c94ab26e9555423d8403aba290fade9e26834
Resul Takak <resultakak@gmail.com>
2021-07-26 21:52:27 +0300

    Update providers.php

--- src/api/config/providers.php
+++ src/api/config/providers.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 use Api\Providers\ConfigProvider;
 
+
+//use App\Providers\ConfigProvider;
+//use App\Providers\LoggerProvider;
+//use App\Providers\ErrorHandlerProvider;
+//use App\Providers\DatabaseProvider;
+//use App\Providers\RequestProvider;
+//use App\Providers\ResponseProvider;
+//use App\Providers\RouterProvider;
+//use App\Providers\CacheDataProvider;
+//use App\Providers\SessionProvider;
+//use App\Providers\ReceiptProvider;
+
 return [
     ConfigProvider::class,
 ];
