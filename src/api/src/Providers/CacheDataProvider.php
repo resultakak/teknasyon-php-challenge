05b3499f6d5c292333797bc72cefcd85d4b0caf5
Resul Takak <resultakak@gmail.com>
2021-07-27 00:44:47 +0300

    Create CacheDataProvider.php

---
+++ src/api/src/Providers/CacheDataProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Providers;
+
+use Phalcon\Cache;
+use Phalcon\Cache\Adapter\Redis;
+use Phalcon\Cache\AdapterFactory;
+use Phalcon\Config;
+use Phalcon\Di\DiInterface;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Storage\SerializerFactory;
+
+class CacheDataProvider implements ServiceProviderInterface
+{
+
+    public function register(DiInterface $container): void
+    {
+        $config = $container->getShared('config');
+
+        $container->setShared(
+            'cache',
+            function () use ($config) {
+                $options = $config->get('cache')->toArray();
+
+                $serializerFactory = new SerializerFactory();
+
+                $adapter = new Redis($serializerFactory, $options);
+
+                return new Cache($adapter);
+            }
+        );
+    }
+}
