a2052b369afe25dcfdb408c37b92be0fa4aed6be
Resul Takak <resultakak@gmail.com>
2021-07-26 21:54:29 +0300

    Create AbstractBootstrap.php

---
+++ src/api/src/Bootstrap/AbstractBootstrap.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Bootstrap;
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Mvc\Micro;
+
+use function microtime;
+
+abstract class AbstractBootstrap
+{
+
+    protected $application;
+
+    protected $container;
+
+    protected $options = [];
+
+    protected $providers = [];
+
+    public function getApplication()
+    {
+        return $this->application;
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    public function getResponse()
+    {
+        return $this->container->getShared('response');
+    }
+
+    abstract public function run();
+
+    public function setup()
+    {
+        $this->container->set('metrics', microtime(true));
+        $this->setupApplication();
+        $this->registerServices();
+    }
+
+    protected function setupApplication()
+    {
+        $this->application = new Micro($this->container);
+        $this->container->setShared('application', $this->application);
+    }
+
+    private function registerServices()
+    {
+        /** @var ServiceProviderInterface $provider */
+        foreach ($this->providers as $provider) {
+            (new $provider)->register($this->container);
+        }
+    }
+}
