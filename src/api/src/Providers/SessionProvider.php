871f4ed15d82815aa7826c531acc216f30cf06bc
Resul Takak <resultakak@gmail.com>
2021-07-27 00:44:53 +0300

    Create SessionProvider.php

---
+++ src/api/src/Providers/SessionProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Providers;
+
+use Phalcon\Di\DiInterface;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Session\Adapter\Redis;
+use Phalcon\Session\Manager;
+use Phalcon\Storage\AdapterFactory;
+use Phalcon\Storage\SerializerFactory;
+
+class SessionProvider implements ServiceProviderInterface
+{
+
+    public function register(DiInterface $container) : void
+    {
+        $config = $container->getShared('config');
+
+        $container->setShared('session', function () use ($config) {
+            $options = $config->get('session')->toArray();
+
+            $session = new Manager;
+            $serializerFactory = new SerializerFactory;
+            $factory = new AdapterFactory($serializerFactory);
+            $redis = new Redis($factory, $options);
+
+            $session->setAdapter($redis)->start();
+
+            return $session;
+        });
+    }
+}
