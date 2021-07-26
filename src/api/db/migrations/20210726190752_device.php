7ee5ff3ea1e81ddf403c8eb967635ccfcaed20f0
Resul Takak <resultakak@gmail.com>
2021-07-26 22:37:21 +0300

    Create 20210726190752_device.php

---
+++ src/api/db/migrations/20210726190752_device.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Device extends AbstractMigration
+{
+    public function change(): void
+    {
+        $table = $this->table('devices');
+        if ($table->exists()) {
+            return;
+        }
+
+        $table
+            ->addColumn('uid', 'string', ['limit' => 60])
+            ->addColumn('app_id', 'string', ['limit' => 60])
+            ->addColumn('language', 'string', ['limit' => 10])
+            ->addColumn('os', 'string', ['limit' => 40])
+            ->addColumn('created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->create();
+    }
+}
