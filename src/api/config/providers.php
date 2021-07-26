108c50524d251c24c43ff8b31b4815b9c3f7b6ae
Resul Takak <resultakak@gmail.com>
2021-07-26 22:03:39 +0300

    Update providers.php

--- src/api/config/providers.php
+++ src/api/config/providers.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use Api\Providers\ConfigProvider;
+use Api\Providers\DatabaseProvider;
 use Api\Providers\RequestProvider;
 use Api\Providers\ResponseProvider;
 use Api\Providers\RouterProvider;
 
 return [
     ConfigProvider::class,
+    DatabaseProvider::class,
     RequestProvider::class,
     ResponseProvider::class,
     RouterProvider::class,
