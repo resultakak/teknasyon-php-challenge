44694b1875d8294e6e662202a85723cdae1a6a9d
Resul Takak <resultakak@gmail.com>
2021-07-26 21:55:18 +0300

    Create RouterProvider.php

---
+++ src/api/src/Providers/RouterProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Providers;
+
+use Api\Controllers\ApiController;
+use Api\Controllers\DefaultController;
+use Api\Middleware\NotFoundMiddleware;
+
+use Phalcon\Di\DiInterface;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Events\Manager;
+use Phalcon\Mvc\Micro;
+use Phalcon\Mvc\Micro\Collection;
+
+class RouterProvider implements ServiceProviderInterface
+{
+
+    public function register(DiInterface $container) : void
+    {
+        $application = $container->getShared('application');
+        $eventsManager = $container->getShared('eventsManager');
+        $this->attachRoutes($application);
+        $this->attachMiddleware($application, $eventsManager);
+        $application->setEventsManager($eventsManager);
+    }
+
+    private function attachRoutes(Micro $application)
+    {
+        $api = new Collection;
+        $api
+            ->setHandler(new ApiController())
+            ->setPrefix('/api')
+            ->post('/register', 'register')
+            ->post('/purchase', 'purchase')
+            ->post('/check_subscription', 'check_subscription')
+        ;
+
+        $application->mount($api);
+    }
+
+    private function attachMiddleware(Micro $application, Manager $eventsManager)
+    {
+        $middleware = $this->getMiddleware();
+
+        foreach ($middleware as $class => $function) {
+            $eventsManager->attach('micro', new $class);
+            $application->{$function}(new $class);
+        }
+    }
+
+    private function getMiddleware() : array
+    {
+        return [
+            NotFoundMiddleware::class => 'before'
+        ];
+    }
+}
