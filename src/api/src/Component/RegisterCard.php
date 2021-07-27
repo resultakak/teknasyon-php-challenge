5f70a18bd7e59684b671f8402ce53059da183e99
Resul Takak <resultakak@gmail.com>
2021-07-27 11:06:12 +0300

    Create RegisterCard.php

---
+++ src/api/src/Component/RegisterCard.php
@@ -0,0 +1,190 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Component;
+use Api\Traits\SerializeTrait;
+
+class RegisterCard implements \JsonSerializable
+{
+    use SerializeTrait;
+
+    /**
+     * @var $uid;
+     */
+    protected $uid;
+
+    /**
+     * @var $app_id
+     */
+    protected $app_id;
+
+    /**
+     * @var $language
+     */
+    protected $language;
+
+    /**
+     * @var $os
+     */
+    protected $os;
+
+    /**
+     * @var $created
+     */
+    protected $created;
+
+    /**
+     * @var $token
+     */
+    private $token;
+
+    /**
+     * RegisterCard constructor.
+     *
+     * @param array|null $data
+     */
+    public function __construct(array $data = null)
+    {
+        $data = json_decode(json_encode($data), true);
+
+        if (!isset($data) || empty($data) || !is_array($data)) {
+            return;
+        }
+
+        if(array_key_exists('uid', $data)) {
+            $this->setUid($data['uid']);
+        }
+
+        if(array_key_exists('app_id', $data)) {
+            $this->setAppId($data['app_id']);
+        }
+
+        if(array_key_exists('language', $data)) {
+            $this->setLanguage($data['language']);
+        }
+
+        if(array_key_exists('os', $data)) {
+            $this->setOs($data['os']);
+        }
+
+        if(array_key_exists('created', $data)) {
+            $this->setCreated($data['created']);
+        }
+
+        if($this->getUid() && $this->getAppId()) {
+            $this->setToken();
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUid(): string
+    {
+        return $this->uid;
+    }
+
+    /**
+     * @param string $uid
+     */
+    public function setUid(string $uid): void
+    {
+        $this->uid = $uid ?? null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAppId(): string
+    {
+        return $this->app_id;
+    }
+
+    /**
+     * @param string $app_id
+     */
+    public function setAppId(string $app_id): void
+    {
+        $this->app_id = $app_id ?? null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param string $language
+     */
+    public function setLanguage(string $language): void
+    {
+        $this->language = $language;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOs(): string
+    {
+        return $this->os;
+    }
+
+    /**
+     * @param string $os
+     */
+    public function setOs(string $os): void
+    {
+        $this->os = $os;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreated(): string
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param string $created
+     */
+    public function setCreated(string $created): void
+    {
+        $this->created = $created;
+    }
+
+    /**
+     *
+     */
+    public function setToken(): void
+    {
+        $this->token = isset($this->uid) && !empty(isset($this->uid)) &&
+        isset($this->app_id) && !empty(isset($this->app_id))
+            ? sha1(json_encode($this))
+            : false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token ?? "";
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'uid'    => sha1($this->encode($this->getUid())),
+            'app_id' => sha1($this->encode($this->getAppId())),
+            'key'    => sha1(getenv('APP_KEY'))
+        ];
+    }
+}
