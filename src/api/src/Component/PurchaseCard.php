911cd33fb2f835376741ac0a4dea73663885858c
Resul Takak <resultakak@gmail.com>
2021-07-27 12:18:49 +0300

    Create PurchaseCard.php

---
+++ src/api/src/Component/PurchaseCard.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Component;
+
+class PurchaseCard
+{
+
+    /**
+     * @var $receipt
+     */
+    public $receipt;
+
+    /**
+     * PurchaseCard constructor.
+     *
+     * @param array|null $data
+     */
+    public function __construct(array $data = null)
+    {
+        $data = json_decode(json_encode($data), true);
+
+        if (!isset($data) || empty($data) || !is_array($data)) {
+            return;
+        }
+
+        if(array_key_exists('receipt', $data)) {
+            $this->setReceipt($data['receipt']);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReceipt(): mixed
+    {
+        return $this->receipt;
+    }
+
+    /**
+     * @param string $receipt
+     */
+    public function setReceipt(string $receipt): void
+    {
+        $this->receipt = $receipt;
+    }
+}
