b3ccebe38e017dd9d45e6388ace9dcc8d451758e
Resul Takak <resultakak@gmail.com>
2021-07-27 11:10:52 +0300

    Create Filters.php

---
+++ src/api/src/Constants/Filters.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Constants;
+
+class Filters
+{
+
+    const FILTER_ABSINT = 'absint';
+
+    const FILTER_ALNUM = 'alnum';
+
+    const FILTER_ALPHA = 'alpha';
+
+    const FILTER_BOOL = 'bool';
+
+    const FILTER_EMAIL = 'email';
+
+    const FILTER_FLOAT = 'float';
+
+    const FILTER_INT = 'int';
+
+    const FILTER_LOWER = 'lower';
+
+    const FILTER_LOWERFIRST = 'lowerFirst';
+
+    const FILTER_REGEX = 'regex';
+
+    const FILTER_REMOVE = 'remove';
+
+    const FILTER_REPLACE = 'replace';
+
+    const FILTER_SPECIAL = 'special';
+
+    const FILTER_SPECIALFULL = 'specialFull';
+
+    const FILTER_STRING = 'string';
+
+    const FILTER_STRIPTAGS = 'striptags';
+
+    const FILTER_TRIM = 'trim';
+
+    const FILTER_UPPER = 'upper';
+
+    const FILTER_UPPERFIRST = 'upperFirst';
+
+    const FILTER_UPPERWORDS = 'upperWords';
+
+    const FILTER_URL = 'url';
+}
