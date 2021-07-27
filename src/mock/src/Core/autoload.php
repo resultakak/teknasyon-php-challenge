46c61761a79786dbf08838599466617d50236ef3
Resul Takak <resultakak@gmail.com>
2021-07-27 18:18:51 +0300

    Update autoload.php

--- src/mock/src/Core/autoload.php
+++ src/mock/src/Core/autoload.php
@@ -8,12 +8,4 @@ use function App\Core\appPath;
 
 require __DIR__.'/functions.php';
 
-$loader = new Loader;
-$namespaces = [
-    //'App\Api\Controllers' => appPath('/api/controllers'),
-];
-
-$loader->registerNamespaces($namespaces);
-$loader->register();
-
 require appPath('/vendor/autoload.php');
