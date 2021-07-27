f1d82bb94fd38ceea0852f9506c1a42383143988
Resul Takak <resultakak@gmail.com>
2021-07-27 18:18:45 +0300

    Create providers.php

---
+++ src/mock/config/providers.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Providers\ConfigProvider;
+use App\Providers\LoggerProvider;
+use App\Providers\ErrorHandlerProvider;
+use App\Providers\DatabaseProvider;
+use App\Providers\RequestProvider;
+use App\Providers\ResponseProvider;
+use App\Providers\RouterProvider;
+use App\Providers\CacheDataProvider;
+use App\Providers\SessionProvider;
+use App\Providers\ReceiptProvider;
+
+return [
+    ConfigProvider::class,
+    LoggerProvider::class,
+    ErrorHandlerProvider::class,
+    DatabaseProvider::class,
+    RequestProvider::class,
+    ResponseProvider::class,
+    RouterProvider::class,
+    CacheDataProvider::class,
+    SessionProvider::class,
+    ReceiptProvider::class,
+];
