5ee6755a1b08d47568671b6af6a2d5e4a1474a8d
Resul Takak <resultakak@gmail.com>
2021-07-27 14:12:03 +0300

    Update Request.php

--- src/api/src/Http/Request.php
+++ src/api/src/Http/Request.php
@@ -5,7 +5,24 @@ declare(strict_types=1);
 namespace Api\Http;
 
 use Phalcon\Http\Request as PhRequest;
+use function str_replace;
 
 class Request extends PhRequest
 {
+
+    /**
+     * @return string
+     */
+    public function getBearerTokenFromHeader(): string
+    {
+        return str_replace('Bearer ', '', $this->getHeader('Authorization'));
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEmptyBearerToken(): bool
+    {
+        return true === empty($this->getBearerTokenFromHeader());
+    }
 }
