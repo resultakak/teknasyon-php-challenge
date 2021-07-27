3700f048641795dffb72a3cdc10b25a008092f00
Resul Takak <resultakak@gmail.com>
2021-07-27 17:08:08 +0300

    Update Applications.php

--- src/api/src/Models/Applications.php
+++ src/api/src/Models/Applications.php
@@ -8,12 +8,6 @@ use Phalcon\Mvc\Model;
 
 class Applications extends Model
 {
-
-    public $id;
-    public $app_id;
-    public $name;
-    public $created;
-
     public function initialize()
     {
         $this->setSource('apps');
