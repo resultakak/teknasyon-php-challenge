0ae5e8b4aae6c3b062d4c1b13a323ca5624e6aeb
Resul Takak <resultakak@gmail.com>
2021-07-27 17:06:10 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ git clone https://github.com/resultakak/php-challenge.git
 cd php-challenge
 cp example.env .env
 docker-compose up -d --build
+
 docker-compose exec api composer install;
-docker-compose exec worker composer install;
-docker-compose exec mock composer install;
+docker-compose exec api vendor/bin/phinx migrate;
 docker-compose exec api vendor/bin/phinx seed:run;
+
+docker-compose exec mock composer install;
+docker-compose exec mock vendor/bin/phinx migrate;
 docker-compose exec mock vendor/bin/phinx seed:run;
+
+docker-compose exec worker composer install;
