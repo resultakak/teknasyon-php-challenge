2e351ff3b04a319495fdc285b238398a3f72c1f4
Resul Takak <resultakak@gmail.com>
2021-07-27 14:48:58 +0300

    Update ApiController.php

--- src/api/src/Controllers/ApiController.php
+++ src/api/src/Controllers/ApiController.php
@@ -9,6 +9,7 @@ use Api\Component\RegisterCard;
 use Api\Http\Response;
 use Api\Models\Devices;
 use Api\Traits\ResponseTrait;
+use Api\Exception\HttpException;
 
 class ApiController extends AbstractController
 {
@@ -16,82 +17,100 @@ class ApiController extends AbstractController
 
     public function register(): Response
     {
-        $postData = $this->request->getJsonRawBody();
+        try {
+            $postData = $this->request->getJsonRawBody();
 
-        $card = new RegisterCard([
-            'uid'      => $this->clean($postData->uid),
-            'app_id'   => $this->clean($postData->app_id),
-            'language' => $this->clean($postData->language),
-            'os'       => $this->clean($postData->os),
-        ]);
+            $card = new RegisterCard([
+                'uid'      => $this->clean($postData->uid),
+                'app_id'   => $this->clean($postData->app_id),
+                'language' => $this->clean($postData->language),
+                'os'       => $this->clean($postData->os),
+            ]);
 
-        $cache_id = 'token_'.md5($card->getToken());
+            $cache_id = 'token_'.md5($card->getToken());
 
-        $token = $this->cache->get($cache_id);
+            $token = $this->cache->get($cache_id);
 
-        if (empty($token)) {
+            if (empty($token)) {
 
-            /*
-             * @TODO APP listesi oluşturulacak
-             */
+                /*
+                 * @TODO APP listesi oluşturulacak
+                 */
 
-            $device = Devices::findFirst(
-                [
-                    'conditions' => 'uid = :uid: AND app_id = :app_id:',
-                    'bind'       => [
-                        'uid'    => $card->getUid(),
-                        'app_id' => $card->getAppId(),
-                    ],
-                ]
-            );
+                $device = Devices::findFirst(
+                    [
+                        'conditions' => 'uid = :uid: AND app_id = :app_id:',
+                        'bind'       => [
+                            'uid'    => $card->getUid(),
+                            'app_id' => $card->getAppId(),
+                        ],
+                    ]
+                );
 
-            if (empty($device)) {
-                $device = new Devices();
-                $device->uid = $card->getUid();
-                $device->app_id = $card->getAppId();
-                $device->language = $card->getLanguage();
-                $device->os = $card->getOs();
+                if (empty($device)) {
+                    $device = new Devices();
+                    $device->uid = $card->getUid();
+                    $device->app_id = $card->getAppId();
+                    $device->language = $card->getLanguage();
+                    $device->os = $card->getOs();
 
-                $result = $device->save();
+                    $result = $device->save();
 
-                if (false === $result) {
-                    $messages = $device->getMessages();
+                    if (false === $result) {
+                        $messages = $device->getMessages();
 
-                    return $this->response
-                        ->setPayloadErrors($messages)
-                        ->setStatusCode($this->response::BAD_REQUEST);
+                        return $this->response
+                            ->setPayloadErrors($messages)
+                            ->setStatusCode($this->response::BAD_REQUEST);
 
+                    }
                 }
+
+                $token = $card->getToken();
+                $this->cache->set($cache_id, $token);
             }
 
-            $token = $card->getToken();
-            $this->cache->set($cache_id, $token);
+            return $this->response
+                ->setPayloadSuccess(['data' => ['token' => $token]])
+                ->setStatusCode($this->response::OK);
+        } catch (HttpException $ex) {
+            return $this->response
+                ->setPayloadError($ex->getMessage())
+                ->setStatusCode($this->response::OK);
         }
-
-        return $this->response
-            ->setPayloadSuccess(['data' => ['token' => $token]])
-            ->setStatusCode($this->response::OK);
     }
 
     public function purchase(): Response
     {
-        $postData = $this->request->getJsonRawBody();
+        try {
+            $postData = $this->request->getJsonRawBody();
 
-        $card = new PurchaseCard([
-            'receipt' => $this->clean($postData->receipt),
-        ]);
+            $card = new PurchaseCard([
+                'receipt' => $this->clean($postData->receipt),
+            ]);
 
-        return $this->response
-            ->setPayloadSuccess(['data' => $card])
-            ->setStatusCode($this->response::OK);
+            return $this->response
+                ->setPayloadSuccess(['data' => $card])
+                ->setStatusCode($this->response::OK);
+        } catch (HttpException $ex) {
+            return $this->response
+                ->setPayloadError($ex->getMessage())
+                ->setStatusCode($this->response::OK);
+        }
     }
 
     public function check_subscription(): Response
     {
-        $token = $this->request->getBearerTokenFromHeader();
+        try {
+            $token = $this->request->getBearerTokenFromHeader();
 
-        return $this->response
-            ->setPayloadSuccess(['data' => $token])
-            ->setStatusCode($this->response::OK);
+            return $this->response
+                ->setPayloadSuccess(['data' => $token])
+                ->setStatusCode($this->response::OK);
+        } catch (HttpException $ex) {
+            return $this->response
+                ->setPayloadError($ex->getMessage())
+                ->setStatusCode($this->response::OK);
+        }
     }
 }
