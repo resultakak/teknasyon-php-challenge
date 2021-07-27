e18c9e7c5e518e5438075839315b58032f9be4cd
Resul Takak <resultakak@gmail.com>
2021-07-27 12:41:29 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git clone https://github.com/resultakak/php-challenge.git
+cd php-challenge
 cp example.env .env
 docker-compose up -d --build
 docker-compose exec api composer install;
