17d12dd4d657e6f0f8874780543819f3f0cf77f3
Resul Takak <resultakak@gmail.com>
2021-07-27 19:45:05 +0300

    Update UserSeeder.php

--- src/mock/db/seeds/UserSeeder.php
+++ src/mock/db/seeds/UserSeeder.php
@@ -1,18 +1,41 @@
 <?php
 
 use Phinx\Seed\AbstractSeed;
+use App\Traits\PasswordTrait;
 
 class UserSeeder extends AbstractSeed
 {
+    use PasswordTrait;
 
     public function run()
     {
         $faker = Faker\Factory::create();
         $data = [];
-        for ($i = 0; $i < 100; $i++) {
+
+        $password = $this->generate(getenv('TEST_PASSWORD'));
+
+        $data[] = [
+            'name'     => 'app_one',
+            'password' => $password,
+            'created'  => date('Y-m-d H:i:s'),
+        ];
+
+        $data[] = [
+            'name'     => 'app_two',
+            'password' => $password,
+            'created'  => date('Y-m-d H:i:s'),
+        ];
+
+        $data[] = [
+            'name'     => 'app_three',
+            'password' => $password,
+            'created'  => date('Y-m-d H:i:s'),
+        ];
+
+        for ($i = 0; $i < 97; $i++) {
             $data[] = [
                 'name'     => $faker->userName,
-                'password' => sha1($faker->password),
+                'password' => $password,
                 'created'  => date('Y-m-d H:i:s'),
             ];
         }
