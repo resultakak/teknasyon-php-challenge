568deede5fa55a27c781557dc1c8dd44412f7a5e
Resul Takak <resultakak@gmail.com>
2021-07-28 18:47:56 +0300

    Update 20210728130000_apps.php

--- src/api/db/migrations/20210728130000_apps.php
+++ src/api/db/migrations/20210728130000_apps.php
@@ -19,6 +19,10 @@ final class Apps extends AbstractMigration
             ->addColumn('password', 'string', ['limit' => 60])
             ->addColumn('name', 'string', ['limit' => 60])
             ->addColumn('created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addIndex(['app_id', 'username'], [
+                'unique' => true,
+                'name'   => 'apps_app_id'
+            ])
             ->create();
     }
 }
