7a09565543703284c0732c7eb067420aa2fb3c94
Resul Takak <resultakak@gmail.com>
2021-07-28 15:28:01 +0300

    Update Apps.php

--- src/api/db/seeds/Apps.php
+++ src/api/db/seeds/Apps.php
@@ -1,30 +1,40 @@
 <?php
 
 use Phinx\Seed\AbstractSeed;
+use Api\Traits\CryptoTrait;
 
 class Apps extends AbstractSeed
 {
+    use CryptoTrait;
 
     public function run()
     {
+        $password = base64_encode($this->encrypt(getenv('TEST_PASSWORD')));
+
         $data = [
             [
-                'id'      => 1,
-                'app_id'  => md5('App 1'),
-                'name'    => 'App 1',
-                'created' => date('Y-m-d H:i:s')
+                'id'       => 1,
+                'app_id'   => md5('App 1'),
+                'username' => 'app_one',
+                'password' => $password,
+                'name'     => 'App 1',
+                'created'  => date('Y-m-d H:i:s')
             ],
             [
-                'id'      => 2,
-                'app_id'  => md5('App 2'),
-                'name'    => 'App 2',
-                'created' => date('Y-m-d H:i:s')
+                'id'       => 2,
+                'app_id'   => md5('App 2'),
+                'username' => 'app_two',
+                'password' => $password,
+                'name'     => 'App 2',
+                'created'  => date('Y-m-d H:i:s')
             ],
             [
-                'id'      => 3,
-                'app_id'  => md5('App 3'),
-                'name'    => 'App 3',
-                'created' => date('Y-m-d H:i:s')
+                'id'       => 3,
+                'app_id'   => md5('App 3'),
+                'username' => 'app_three',
+                'password' => $password,
+                'name'     => 'App 3',
+                'created'  => date('Y-m-d H:i:s')
             ]
         ];
 
