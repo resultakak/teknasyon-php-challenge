5227a28276156c3c58afa587c99d39157a80c78f
Resul Takak <resultakak@gmail.com>
2021-07-28 20:52:58 +0300

    Create CacheManagerProvider.php

---
+++ src/api/src/Providers/CacheManagerProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Providers;
+
+use Api\Cache\Manager;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Di\DiInterface;
+
+class CacheManagerProvider implements ServiceProviderInterface
+{
+    /**
+     * @param DiInterface $container
+     */
+    public function register(DiInterface $container): void
+    {
+        $application = $container->getShared('application');
+
+        $container->setShared('cacheManager', function () use ($application) {
+            $cacheManager = new Manager($application);
+
+            return $cacheManager;
+        });
+    }
+}
