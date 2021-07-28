a8f0ab08d3dfe10ab84596eaa9a4d4bfa5952032
Resul Takak <resultakak@gmail.com>
2021-07-28 15:27:55 +0300

    Create 20210728130000_apps.php

---
+++ src/api/db/migrations/20210728130000_apps.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Apps extends AbstractMigration
+{
+    public function change(): void
+    {
+        $table = $this->table('apps');
+        if ($table->exists()) {
+            return;
+        }
+
+        $table
+            ->addColumn('app_id', 'string', ['limit' => 60])
+            ->addColumn('username', 'string', ['limit' => 60])
+            ->addColumn('password', 'string', ['limit' => 60])
+            ->addColumn('name', 'string', ['limit' => 60])
+            ->addColumn('created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->create();
+    }
+}
