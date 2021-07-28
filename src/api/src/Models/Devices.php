07fc257fac048bf294672df92a45b538f716ecd3
Resul Takak <resultakak@gmail.com>
2021-07-29 01:04:33 +0300

    Update Devices.php

--- src/api/src/Models/Devices.php
+++ src/api/src/Models/Devices.php
@@ -9,12 +9,37 @@ use Phalcon\Mvc\Model\Behavior\Timestampable;
 use Phalcon\Validation;
 use Phalcon\Validation\Validator\StringLength;
 
+/**
+ * Class Devices
+ *
+ * @method void setSource(string)
+ * @method void addBehavior()
+ */
 class Devices extends Model
 {
+    /**
+     * @var string $uid
+     */
     public $uid;
+
+    /**
+     * @var string $app_id
+     */
     public $app_id;
+
+    /**
+     * @var string $language
+     */
     public $language;
+
+    /**
+     * @var string $token
+     */
     public $token;
+
+    /**
+     * @var string $created
+     */
     public $created;
 
     public function initialize()
@@ -37,56 +62,61 @@ class Devices extends Model
     {
         $validator = new Validation();
 
-        $validator->add([
-            "uid",
-            "app_id",
-            "language",
-            "os",
-            "token",
-        ], new StringLength([
-            "max"             => [
-                "uid"      => 60,
-                "app_id"   => 60,
-                "language" => 10,
-                "os"       => 30,
-                "token"    => 35,
+        $validator->add(
+            [
+                "uid",
+                "app_id",
+                "language",
+                "os",
+                "token",
             ],
-            "min"             => [
-                "uid"      => 20,
-                "app_id"   => 20,
-                "language" => 1,
-                "os"       => 3,
-                "token"    => 30,
-            ],
-            "messageMaximum"  => [
-                "uid"      => "Uid too long",
-                "app_id"   => "AppID too long",
-                "language" => "Language code too long",
-                "os"       => "OS name too long",
-                "token"    => "Internal Server Error",
-            ],
-            "messageMinimum"  => [
-                "uid"      => "Uid too short",
-                "app_id"   => "AppID too short",
-                "language" => "Language code too short",
-                "os"       => "OS name too short",
-                "token"    => "Internal Server Error",
-            ],
-            "includedMaximum" => [
-                "uid"      => false,
-                "app_id"   => true,
-                "language" => false,
-                "os"       => false,
-                "token"    => false,
-            ],
-            "includedMinimum" => [
-                "uid"      => false,
-                "app_id"   => true,
-                "language" => false,
-                "os"       => false,
-                "token"    => false,
-            ]
-        ]));
+            new StringLength(
+                [
+                    "max"             => [
+                        "uid"      => 60,
+                        "app_id"   => 60,
+                        "language" => 10,
+                        "os"       => 30,
+                        "token"    => 35,
+                    ],
+                    "min"             => [
+                        "uid"      => 20,
+                        "app_id"   => 20,
+                        "language" => 1,
+                        "os"       => 3,
+                        "token"    => 30,
+                    ],
+                    "messageMaximum"  => [
+                        "uid"      => "Uid too long",
+                        "app_id"   => "AppID too long",
+                        "language" => "Language code too long",
+                        "os"       => "OS name too long",
+                        "token"    => "Internal Server Error",
+                    ],
+                    "messageMinimum"  => [
+                        "uid"      => "Uid too short",
+                        "app_id"   => "AppID too short",
+                        "language" => "Language code too short",
+                        "os"       => "OS name too short",
+                        "token"    => "Internal Server Error",
+                    ],
+                    "includedMaximum" => [
+                        "uid"      => false,
+                        "app_id"   => true,
+                        "language" => false,
+                        "os"       => false,
+                        "token"    => false,
+                    ],
+                    "includedMinimum" => [
+                        "uid"      => false,
+                        "app_id"   => true,
+                        "language" => false,
+                        "os"       => false,
+                        "token"    => false,
+                    ]
+                ]
+            )
+        );
 
         return $this->validate($validator);
     }
