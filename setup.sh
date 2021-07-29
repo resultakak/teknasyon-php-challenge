32bb32886f7892f2f6ee295aa158e4f7b2c3f497
Resul Takak <resultakak@gmail.com>
2021-07-29 21:01:17 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp example.env .env
-docker build -t resultakak/php:latest ./build/php
+docker build -t resultakak/php:mavi ./build/php
 docker-compose up -d --build
 docker-compose exec api make setup;
 docker-compose exec mock make setup;
