b64999fb9f7cfaf8b2cf29e01befa8e304e51a05
Resul Takak <resultakak@gmail.com>
2021-07-29 20:41:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# curl -s https://raw.githubusercontent.com/resultakak/php-challenge/develop/setup.sh | bash
-
 git clone https://github.com/resultakak/php-challenge.git
-
 cd php-challenge
-
 chmod +x setup.sh
-
 bash setup.sh
