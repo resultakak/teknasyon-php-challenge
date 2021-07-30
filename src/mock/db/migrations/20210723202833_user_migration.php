94948deb0f0f51c8e8b67635828e54dfd58907d2
Resul Takak <resultakak@gmail.com>
2021-07-30 21:09:55 +0300

    Update 20210723202833_user_migration.php

--- src/mock/db/migrations/20210723202833_user_migration.php
+++ src/mock/db/migrations/20210723202833_user_migration.php
@@ -8,6 +8,10 @@ final class UserMigration extends AbstractMigration
     public function change(): void
     {
         $table = $this->table('credentials');
+        if ($table->exists()) {
+            return;
+        }
+
         $table
             ->addColumn('name', 'string', ['limit' => 80])
             ->addColumn('password', 'string', ['limit' => 120])
