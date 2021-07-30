e73a17c33ecd2f631837a4c99045486db807e9b5
Resul Takak <resultakak@gmail.com>
2021-07-30 16:30:14 +0300

    Create 20210729211310_app_credentials.php

---
+++ src/api/db/migrations/20210729211310_app_credentials.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+final class AppCredentials extends AbstractMigration
+{
+    public function change(): void
+    {
+        $table = $this->table('app_credentials', ['id' => 'acid']);
+        if ($table->exists()) {
+            return;
+        }
+
+        $table
+            ->addColumn('aid', 'integer', ['limit' => MysqlAdapter::INT_BIG])
+            ->addColumn('username', 'string', ['limit' => 70])
+            ->addColumn('password', 'string', ['limit' => 70])
+            ->addColumn('platform', 'enum', ['values' => ['IOS', 'ANDROID']])
+            ->addColumn('created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addIndex(['aid', 'username'], [
+                'unique' => true,
+                'name'   => 'acid_aid'
+            ])
+            ->addForeignKey(
+                'aid',
+                'apps',
+                'aid',
+                ['update' => 'NO_ACTION']
+            )
+            ->create();
+    }
+}
