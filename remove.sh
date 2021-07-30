4e2488ba68be9bc3224a475227a31937e4fcff6f
Resul Takak <resultakak@gmail.com>
2021-07-30 16:26:05 +0300

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker-compose down
+docker system prune -a --volumes
+rm ./src/api/logs/*.log
+rm ./src/api/composer.lock
+rm -rf ./src/api/vendor
+rm ./src/mock/logs/*.log
+rm ./src/mock/composer.lock
+rm -rf ./src/mock/vendor
+rm ./src/worker/*.log
+rm ./src/worker/storage/logs/*.log
+rm -rf ./src/worker/vendor
+rm ./src/worker/composer.lock
+
+#rm -rf ./build/mysql/data/
