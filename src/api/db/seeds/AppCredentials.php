9d1df52b2dd5def0dfea75e2bd42c3650a2bd576
Resul Takak <resultakak@gmail.com>
2021-07-30 16:32:13 +0300

    Create AppCredentials.php

---
+++ src/api/db/seeds/AppCredentials.php
@@ -0,0 +1,43 @@
+<?php
+
+use Api\Traits\CryptoTrait;
+use Phinx\Seed\AbstractSeed;
+
+class AppCredentials extends AbstractSeed
+{
+    use CryptoTrait;
+
+    public function run()
+    {
+        $password = $this->encrypt(getenv('TEST_PASSWORD'));
+
+        $data = [
+            [
+                'acid'     => 1,
+                'aid'      => 1,
+                'username' => 'app_one',
+                'password' => $password,
+                'platform' => (0 === (random_int(1,12) % 2)) ? 'IOS' : 'ANDROID',
+                'created'  => date('Y-m-d H:i:s')
+            ],
+            [
+                'acid'     => 2,
+                'aid'      => 2,
+                'username' => 'app_two',
+                'password' => $password,
+                'platform' => (0 === (random_int(1,12) % 2)) ? 'IOS' : 'ANDROID',
+                'created'  => date('Y-m-d H:i:s')
+            ],
+            [
+                'acid'     => 3,
+                'aid'      => 3,
+                'username' => 'app_three',
+                'password' => $password,
+                'platform' => (0 === (random_int(1,12) % 2)) ? 'IOS' : 'ANDROID',
+                'created'  => date('Y-m-d H:i:s')
+            ]
+        ];
+
+        $this->table('app_credentials')->insert($data)->saveData();
+    }
+}
