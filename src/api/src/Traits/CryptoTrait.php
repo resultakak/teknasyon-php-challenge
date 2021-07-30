883c52af84c340fb11203ec7dff410c012ae423b
Resul Takak <resultakak@gmail.com>
2021-07-30 22:59:11 +0300

    Update CryptoTrait.php

--- src/api/src/Traits/CryptoTrait.php
+++ src/api/src/Traits/CryptoTrait.php
@@ -5,19 +5,16 @@ declare(strict_types=1);
 namespace Api\Traits;
 
 use Phalcon\Crypt;
-use function getenv;
+
 use function base64_encode;
+use function getenv;
 
 trait CryptoTrait
 {
     /**
-     * @return false|string
+     * @param string $text
+     * @return string
      */
-    private function getKey()
-    {
-        return base64_decode(getenv('APP_KEY'));
-    }
-
     protected function encrypt(string $text): string
     {
         /** @var Crypt $crypt */
@@ -27,7 +24,18 @@ trait CryptoTrait
         return base64_encode($crypt->encrypt($text, $key));
     }
 
+    /**
+     * @return false|string
+     */
+    private function getKey()
+    {
+        return base64_decode(getenv('APP_KEY'));
+    }
 
+    /**
+     * @param string $encrypted
+     * @return mixed
+     */
     protected function decrypt(string $encrypted)
     {
         /** @var Crypt $crypt */
